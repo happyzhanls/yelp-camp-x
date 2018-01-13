fca51a98e6979397af966a1597f88518dd0439dd
Daniel Zhan <happyzhanls@gmail.com>
2018-01-13 08:26:11 +0000

    Add contact route

--- routes/contact.js
+++ routes/contact.js
@@ -19,8 +19,7 @@ router.post("/send", function(req, res) {
     // secret key
     var secretKey = process.env.CAPTCHA;
     // Verify URL
-    var verifyURL = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${captcha}&remoteip=${req
-      .connection.remoteAddress}`;
+    var verifyURL = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${captcha}&remoteip=${req.connection.remoteAddress}`;
     // Make request to Verify URL
     request.get(verifyURL, function(err, response, body) {
       if (err) return console.log(err);
@@ -29,33 +28,33 @@ router.post("/send", function(req, res) {
         req.flash("error", "Captcha Failed");
         return res.redirect("/contact");
       }
-        var smtpTransport = nodemailer.createTransport({
-            service: 'Gmail', 
-            auth: {
-              user: 'happyzhanls@gmail.com',
-              pass: process.env.GMAILPW
-            }
-        });
-        
-        var mailOptions = {
-            from: 'happyzhanls@gmail.com',
-            to: 'happyzhanls@gmail.com',
-            replyTo: req.body.email,
-            subject: "Let's Camp contact request from: " + req.body.name,
-            text: 'You have received an email from... Name: '+ req.body.name + ' Email: ' + req.body.email + ' Message: ' + req.body.message,
-            html: '<h3>You have received an email from...</h3><ul><li>Name: ' + req.body.name + ' </li><li>Email: ' + req.body.email + ' </li></ul><p>Message: <br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + req.body.message + ' </p>'
-        };
-        
-        smtpTransport.sendMail(mailOptions, function(err, info){
-          if(err) {
-            console.log(err);
-            req.flash("error", "Something went wrong... Please try again later!");
-            res.redirect("/contact");
-          } else {
-            req.flash("success", "Your email has been sent, I will respond within 24 hours.");
-            res.redirect("/campgrounds");
+      var smtpTransport = nodemailer.createTransport({
+          service: 'Gmail', 
+          auth: {
+            user: 'happyzhanls@gmail.com',
+            pass: process.env.GMAILPW
           }
-        });
+      });
+      
+      var mailOptions = {
+          from: 'happyzhanls@gmail.com',
+          to: 'happyzhanls@gmail.com',
+          replyTo: req.body.email,
+          subject: "Let's Camp contact request from: " + req.body.name,
+          text: 'You have received an email from... Name: '+ req.body.name + ' Email: ' + req.body.email + ' Message: ' + req.body.message,
+          html: '<h3>You have received an email from...</h3><ul><li>Name: ' + req.body.name + ' </li><li>Email: ' + req.body.email + ' </li></ul><p>Message: <br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + req.body.message + ' </p>'
+      };
+      
+      smtpTransport.sendMail(mailOptions, function(err, info){
+        if(err) {
+          console.log(err);
+          req.flash("error", "Something went wrong... Please try again later!");
+          res.redirect("/contact");
+        } else {
+          req.flash("success", "Your email has been sent, I will respond within 24 hours.");
+          res.redirect("/campgrounds");
+        }
+      });
     });
 });
 
