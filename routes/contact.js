0bc3cc749a455eca6abc0a260fff0cbff7725ef4
Daniel Zhan <happyzhanls@gmail.com>
2018-01-13 07:25:52 +0000

    debug

--- routes/contact.js
+++ routes/contact.js
@@ -38,12 +38,12 @@ router.post("/send", function(req, res) {
         });
         
         var mailOptions = {
-            from: 'email@domain.com',
-            to: 'email@domain.com',
+            from: 'happyzhanls@gmail.com',
+            to: 'happyzhanls@gmail.com',
             replyTo: req.body.email,
             subject: "Let's Camp contact request from: " + req.body.name,
             text: 'You have received an email from... Name: '+ req.body.name + ' Email: ' + req.body.email + ' Message: ' + req.body.message,
-            html: '<h3>You have received an email from...</h3><ul><li>Name: ' + req.body.name + ' </li><li>Phone: ' + req.body.phone + ' </li><li>Email: ' + req.body.email + ' </li></ul><p>Message: <br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + req.body.message + ' </p>'
+            html: '<h3>You have received an email from...</h3><ul><li>Name: ' + req.body.name + ' </li><li>Email: ' + req.body.email + ' </li></ul><p>Message: <br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + req.body.message + ' </p>'
         };
         
         smtpTransport.sendMail(mailOptions, function(err, info){
@@ -52,7 +52,7 @@ router.post("/send", function(req, res) {
             req.flash("error", "Something went wrong... Please try again later!");
             res.redirect("/contact");
           } else {
-            req.flash("success", "Your email has been sent, we will respond within 24 hours.");
+            req.flash("success", "Your email has been sent, I will respond within 24 hours.");
             res.redirect("/campgrounds");
           }
         });
