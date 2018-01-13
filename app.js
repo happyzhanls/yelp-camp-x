09f9483d8bbed715ead1fb539915fb7fa8c44d84
Daniel Zhan <happyzhanls@gmail.com>
2018-01-13 06:22:42 +0000

    Add connect-flash

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require("express"),
     mongoose = require("mongoose"),
     passport = require("passport"),
     moment = require("moment"),
+    flash = require("connect-flash"),
     methodOverride = require("method-override"),
     User = require("./models/user"),
     seeds = require("./seeds");
@@ -12,7 +13,8 @@ var express = require("express"),
 // require routes
 var campgroundRoutes = require("./routes/campgrounds"),
     commentRoutes    = require("./routes/comments"),
-    indexRoutes      = require("./routes/index");
+    indexRoutes      = require("./routes/index"),
+    contactRoutes    = require("./routes/contact");
 
 // setup mongodb connection.
 var databaseURL = process.env.DATABASEURL || "mongodb://localhost/YelpCamp";
@@ -22,6 +24,7 @@ mongoose.connect(databaseURL);
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(flash());
 app.use(express.static(__dirname + "/public/"));
 app.locals.moment = moment;
 
@@ -51,6 +54,7 @@ app.use(function(req, res, next){
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
+app.use("/contact", contactRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Server started!");
