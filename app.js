6136cbbba67aff36e12386a0abca4c7add31e93d
Daniel Zhan <happyzhanls@gmail.com>
2018-01-15 05:26:25 +0000

    Add two global varables for flash message

--- app.js
+++ app.js
@@ -47,6 +47,8 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
+    res.locals.success = req.flash("success");
+    res.locals.failure = req.flash("failure");
     next();
 });
 
